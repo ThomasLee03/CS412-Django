b54e52bf3c8e6b17b53edec3c90fc77ffff8712d
ThomasLee03 <tlee03@bu.edu>
2024-11-03 12:41:38 -0500

    modified logout and added a9_url.txt

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path(r'', views.ShowAllProfilesView.as_view(), name = "show_all"),
     path('profile/<int:pk>', views.ShowProfilePageView.as_view(), name='profile'),
     path('create_profile', views.CreateProfileView.as_view(), name='create_profile'), ### NEW
-    path('profile/create_status', views.CreateStatusMessageView.as_view(), name='create_status'), ### NEW
+    path('status/create_status', views.CreateStatusMessageView.as_view(), name='create_status'), ### NEW
     path('profile/update', views.UpdateProfileView.as_view(), name='update_profile'), ### NEW
     path('status/<int:pk>/delete', views.DeleteStatusMessageView.as_view(), name='delete_status'), ### NEW
     path('status/<int:pk>/update', views.UpdateStatusMessageView.as_view(), name='update_status'), ### NEW
@@ -25,6 +25,6 @@ urlpatterns = [
     path('profile/news_feed', views.ShowNewsFeedView.as_view(), name='news_feed'),
     #note that r mean regular expression
     path('login/', auth_views.LoginView.as_view(template_name = 'mini_fb/login.html'), name = "login"),
-    #path('logout/', auth_views.LogoutView.as_view(next_page='show_all'), name = "logout"),
-    path('logout/', views.custom_logout, name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='mini_fb/logged_out.html'), name = "logout"),
+    #path('logout/', views.custom_logout, name='logout'),
 ]
